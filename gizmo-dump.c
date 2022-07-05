a3b57e2cad3e941738d623579be674e82a3b7ca6
Alexei A. Smekalkine <ikle@ikle.ru>
2022-07-06 00:38:08 +0300

    gizmo-dump: dump binary attributes in base64 form

--- gizmo-dump.c
+++ gizmo-dump.c
@@ -8,12 +8,33 @@
 
 #include <gizmo-misc.h>
 
+#include "base64.h"
 #include "gizmo-int.h"
 
 static void sep (const void *o, FILE *to)
 {
 	if (o != NULL)
 		putc ('\n', to);
+}
+
+static void dump_attr (const char *name, const char *data, size_t len, FILE *to)
+{
+	struct b64_filter b64;
+	size_t i;
+
+	if (strstr (name, ";binary") == NULL) {
+		fprintf (to, "%s: %.*s\n", name, (int) len, data);
+		return;
+	}
+
+	fprintf (to, "%s::\n ", name);
+	b64_init (&b64, to, "\n ");
+
+	for (i = 0; i < len; ++i)
+		b64_putc (&b64, data[i]);
+
+	b64_fini (&b64);
+	fprintf (to, "\n");
 }
 
 static void dump_attrs (const struct gizmo *o, LDAPMessage *e, FILE *to)
@@ -35,8 +56,7 @@ static void dump_attrs (const struct gizmo *o, LDAPMessage *e, FILE *to)
 		vals = ldap_get_values_len (o->ldap, e, name);
 
 		for (i = 0; vals[i] != NULL; ++i)
-			fprintf (to, "%s: %.*s\n", name,
-				 (int) vals[i]->bv_len, vals[i]->bv_val);
+			dump_attr (name, vals[i]->bv_val, vals[i]->bv_len, to);
 
 		ldap_value_free_len (vals);
 	}
